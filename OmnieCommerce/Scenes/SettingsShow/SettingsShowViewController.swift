72f4c941936e8f8fe992e7703382695ce0de2df0
SerhiiM <msm72@mail.ru>
2016-11-17 09:57:58 +0200

    change SettingsShowViewController to setup Top Bar View

--- OmnieCommerce/Scenes/SettingsShow/SettingsShowViewController.swift
+++ OmnieCommerce/Scenes/SettingsShow/SettingsShowViewController.swift
@@ -26,6 +26,15 @@ class SettingsShowViewController: BaseViewController, SettingsShowViewController
     var router: SettingsShowRouter!
     
     @IBOutlet weak var topBarView: TopBarView!
+    @IBOutlet weak var topBarViewHeightPortraitConstraint: NSLayoutConstraint!
+    
+    override var topBarViewRounding: CircleView.CirleRadius {
+        willSet {
+            if (newValue == .big) {
+                topBarViewHeightPortraitConstraint.constant = Config.Constants.topViewBarHeightBig
+            }
+        }
+    }
 
     
     // MARK: - Class initialization
@@ -40,7 +49,9 @@ class SettingsShowViewController: BaseViewController, SettingsShowViewController
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        setup()
+        // Config topBarView
+        topBarViewRounding = .big
+        setup(topBarView: topBarView)
         
         doSomethingOnLoad()
     }
