cb0be6bae0ae64d3e7e4065d80eb9c7586197d5e
SerhiiM <msm72@mail.ru>
2016-11-28 19:17:28 +0200

    change NewsShowVC & storyboard

--- OmnieCommerce/Scenes/NewsShow/NewsShowViewController.swift
+++ OmnieCommerce/Scenes/NewsShow/NewsShowViewController.swift
@@ -27,15 +27,6 @@ class NewsShowViewController: BaseViewController, NewsShowViewControllerInput {
     
     @IBOutlet weak var smallTopBarView: SmallTopBarView!
     @IBOutlet weak var copyrightLabel: CustomLabel!
-    @IBOutlet weak var topBarViewHeightPortraitConstraint: NSLayoutConstraint!
-    
-//    override var topBarViewRounding: String {
-//        willSet {
-//            if (newValue == "Big") {
-//                topBarViewHeightPortraitConstraint.constant = Config.Constants.topViewBarHeightBig
-//            }
-//        }
-//    }
 
     
     // MARK: - Class Initialization
@@ -51,7 +42,8 @@ class NewsShowViewController: BaseViewController, NewsShowViewControllerInput {
         super.viewDidLoad()
         
         // Config topBarView
-//        topBarViewRounding = "Big"
+        smallTopBarView.type = "ParentSearch"
+        topBarViewStyle = .Small
         setup(topBarView: smallTopBarView)
 
         doSomethingOnLoad()
@@ -60,6 +52,8 @@ class NewsShowViewController: BaseViewController, NewsShowViewControllerInput {
 
     // MARK: - Custom Functions
     func doSomethingOnLoad() {
+        print(object: "\(type(of: self)): \(#function) run.")
+        
         // NOTE: Ask the Interactor to do some work
         let request = NewsShow.Something.Request()
         output.doSomething(request: request)
@@ -67,7 +61,24 @@ class NewsShowViewController: BaseViewController, NewsShowViewControllerInput {
     
     // Display logic
     func displaySomething(viewModel: NewsShow.Something.ViewModel) {
+        print(object: "\(type(of: self)): \(#function) run.")
+        
         // NOTE: Display the result from the Presenter
         // nameTextField.text = viewModel.name
+    }
+    
+    func setupScene(withSize size: CGSize) {
+        print(object: "\(type(of: self)): \(#function) run. Screen view size = \(size)")
+        
+        smallTopBarView.setNeedsDisplay()
+        smallTopBarView.circleView.setNeedsDisplay()
+    }
+    
+    
+    // MARK: - Transition
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        print(object: "\(type(of: self)): \(#function) run. New size = \(size)")
+        
+        setupScene(withSize: size)
     }
 }
