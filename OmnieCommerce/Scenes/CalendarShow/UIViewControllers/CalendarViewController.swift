2f89c9c0afd7294156409ab79e1424129a250107
SerhiiM <msm72@mail.ru>
2016-12-08 17:31:58 +0200

    add CVCalendar framework

--- OmnieCommerce/Scenes/CalendarShow/UIViewControllers/CalendarViewController.swift
+++ OmnieCommerce/Scenes/CalendarShow/UIViewControllers/CalendarViewController.swift
@@ -7,29 +7,242 @@
 //
 
 import UIKit
+import CVCalendar
 
-class CalendarViewController: UIViewController {
+class CalendarViewController: BaseViewController {
+    struct Color {
+        static let selectedText = UIColor.white
+        static let text = UIColor.black
+        static let textDisabled = UIColor.gray
+        static let selectionBackground = UIColor(red: 0.2, green: 0.2, blue: 1.0, alpha: 1.0)
+        static let sundayText = UIColor(red: 1.0, green: 0.2, blue: 0.2, alpha: 1.0)
+        static let sundayTextDisabled = UIColor(red: 1.0, green: 0.6, blue: 0.6, alpha: 1.0)
+        static let sundaySelectionBackground = sundayText
+    }
 
+    
+    // MARK: - Properties
+    var selectedDay: DayView!
+
+    @IBOutlet weak var menuView: CVCalendarMenuView!
+    @IBOutlet weak var calendarView: CVCalendarView!
+    
+    
+    override func viewDidLayoutSubviews() {
+        print(object: "\(type(of: self)): \(#function) run.")
+        super.viewDidLayoutSubviews()
+        
+        calendarView.commitCalendarViewUpdate()
+        menuView.commitMenuViewUpdate()
+    }
+
+    override func viewWillLayoutSubviews() {
+        print(object: "\(type(of: self)): \(#function) run.")
+        super.viewWillLayoutSubviews()
+        
+    }
+    
+    // MARK: - Class Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Delegates
+        menuView.delegate = self
+        calendarView.delegate = self
+        calendarView.calendarAppearanceDelegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+// MARK: - CVCalendarViewDelegate
+extension CalendarViewController: CVCalendarViewDelegate {
+    func presentationMode() -> CalendarMode {
+        return .monthView
     }
-    */
+    
+    func shouldShowWeekdaysOut() -> Bool {
+        return true
+    }
 
+    func shouldAnimateResizing() -> Bool {
+        return true // Default value is true
+    }
+
+    private func shouldSelectDayView(dayView: DayView) -> Bool {
+        return true
+    }
+    
+    func shouldAutoSelectDayOnMonthChange() -> Bool {
+        return false
+    }
+    
+    func didSelectDayView(_ dayView: CVCalendarDayView, animationDidFinish: Bool) {
+        selectedDay = dayView
+    }
+    
+    func shouldSelectRange() -> Bool {
+        return true
+    }
+    
+    func didSelectRange(from startDayView: DayView, to endDayView: DayView) {
+        print(object: "RANGE SELECTED: \(startDayView.date.commonDescription) to \(endDayView.date.commonDescription)")
+    }
+    
+    func preliminaryView(viewOnDayView dayView: DayView) -> UIView {
+        let circleView = CVAuxiliaryView(dayView: dayView, rect: dayView.frame, shape: CVShape.circle)
+        circleView.borderColor = (Config.Constants.isAppThemesLight) ? UIColor.green : UIColor.darkCyan
+        
+        return circleView
+    }
+
+    func preliminaryView(shouldDisplayOnDayView dayView: DayView) -> Bool {
+        if (dayView.isCurrentDay) {
+            return true
+        }
+        
+        return false
+    }
+    
+    func disableScrollingBeforeDate() -> Date {
+        return Date()
+    }
+    
+    func maxSelectableRange() -> Int {
+        return 1
+    }
+    
+    func earliestSelectableDate() -> Date {
+        return Date()
+    }
+    
+    func latestSelectableDate() -> Date {
+        var dayComponents = DateComponents()
+        dayComponents.day = 70
+        let calendar = Calendar(identifier: .gregorian)
+        
+        if let lastDate = calendar.date(byAdding: dayComponents, to: Date()) {
+            return lastDate
+        } else {
+            return Date()
+        }
+    }
+    
+    func supplementaryView(viewOnDayView dayView: DayView) -> UIView {
+        
+        dayView.setNeedsLayout()
+        dayView.layoutIfNeeded()
+        
+        let π = M_PI
+        
+        let ringSpacing: CGFloat = 3.0
+        let ringInsetWidth: CGFloat = 1.0
+        let ringVerticalOffset: CGFloat = 1.0
+        var ringLayer: CAShapeLayer!
+        let ringLineWidth: CGFloat = 4.0
+        let ringLineColour: UIColor = .blue
+        
+        let newView = UIView(frame: dayView.frame)
+        
+        let diameter: CGFloat = (min(newView.bounds.width, newView.bounds.height)) - ringSpacing
+        let radius: CGFloat = diameter / 2.0
+        
+        let rect = CGRect(x: newView.frame.midX-radius, y: newView.frame.midY-radius-ringVerticalOffset, width: diameter, height: diameter)
+        
+        ringLayer = CAShapeLayer()
+        newView.layer.addSublayer(ringLayer)
+        
+        ringLayer.fillColor = nil
+        ringLayer.lineWidth = ringLineWidth
+        ringLayer.strokeColor = ringLineColour.cgColor
+        
+        let ringLineWidthInset: CGFloat = CGFloat(ringLineWidth/2.0) + ringInsetWidth
+        let ringRect: CGRect = rect.insetBy(dx: ringLineWidthInset, dy: ringLineWidthInset)
+        let centrePoint: CGPoint = CGPoint(x: ringRect.midX, y: ringRect.midY)
+        let startAngle: CGFloat = CGFloat(-π/2.0)
+        let endAngle: CGFloat = CGFloat(π * 2.0) + startAngle
+        let ringPath: UIBezierPath = UIBezierPath(arcCenter: centrePoint, radius: ringRect.width/2.0, startAngle: startAngle, endAngle: endAngle, clockwise: true)
+        
+        ringLayer.path = ringPath.cgPath
+        ringLayer.frame = newView.layer.bounds
+        
+        return newView
+    }
+
+}
+
+
+// MARK: - CVCalendarViewAppearanceDelegate
+extension CalendarViewController: CVCalendarViewAppearanceDelegate {
+    func dayLabelWeekdayDisabledColor() -> UIColor {
+        return UIColor.yellow
+    }
+    
+    func dayLabelPresentWeekdayInitallyBold() -> Bool {
+        return false
+    }
+    
+    func spaceBetweenDayViews() -> CGFloat {
+        return 10
+    }
+    
+    func dayLabelFont(by weekDay: Weekday, status: CVStatus, present: CVPresent) -> UIFont { return UIFont.systemFont(ofSize: 14) }
+    
+    func dayLabelColor(by weekDay: Weekday, status: CVStatus, present: CVPresent) -> UIColor? {
+        switch (weekDay, status, present) {
+        case (_, .selected, _), (_, .highlighted, _): return Color.selectedText
+        case (.sunday, .in, _): return Color.sundayText
+        case (.sunday, _, _): return Color.sundayTextDisabled
+        case (_, .in, _): return Color.text
+        default: return Color.textDisabled
+        }
+    }
+    
+    func dayLabelBackgroundColor(by weekDay: Weekday, status: CVStatus, present: CVPresent) -> UIColor? {
+        switch (weekDay, status, present) {
+        case (.sunday, .selected, _), (.sunday, .highlighted, _): return Color.sundaySelectionBackground
+        case (_, .selected, _), (_, .highlighted, _): return Color.selectionBackground
+        default: return nil
+        }
+    }
+}
+
+
+// MARK: - CVCalendarMenuViewDelegate
+extension CalendarViewController: CVCalendarMenuViewDelegate {
+    func firstWeekday() -> Weekday {
+        return .monday
+    }
+    
+    func dayOfWeekTextColor(by weekday: Weekday) -> UIColor {
+        return weekday == .sunday ? UIColor.red : UIColor.veryLightGray
+    }
+    
+    func dayOfWeekTextColor() -> UIColor {
+        return UIColor.darkCyan
+    }
+    
+    func dayOfWeekBackGroundColor() -> UIColor {
+        return UIColor.blue
+    }
+
+    func dayOfWeekBackGroundColor(by weekday: Weekday) -> UIColor {
+        return UIColor.clear
+    }
+    
+    func dayOfWeekTextUppercase() -> Bool {
+        return false
+    }
+    
+    func dayOfWeekFont() -> UIFont {
+        return UIFont.ubuntuLight16
+    }
+    
+    func weekdaySymbolType() -> WeekdaySymbolType {
+        return .short
+    }
 }
--- Pods/Target Support Files/Pods-OmnieCommerce/Pods-OmnieCommerce-frameworks.sh
+++ Pods/Target Support Files/Pods-OmnieCommerce/Pods-OmnieCommerce-frameworks.sh
@@ -59,8 +59,13 @@ code_sign_if_enabled() {
   if [ -n "${EXPANDED_CODE_SIGN_IDENTITY}" -a "${CODE_SIGNING_REQUIRED}" != "NO" -a "${CODE_SIGNING_ALLOWED}" != "NO" ]; then
     # Use the current code_sign_identitiy
     echo "Code Signing $1 with Identity ${EXPANDED_CODE_SIGN_IDENTITY_NAME}"
-    echo "/usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} ${OTHER_CODE_SIGN_FLAGS} --preserve-metadata=identifier,entitlements \"$1\""
-    /usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} ${OTHER_CODE_SIGN_FLAGS} --preserve-metadata=identifier,entitlements "$1"
+    local code_sign_cmd="/usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} ${OTHER_CODE_SIGN_FLAGS} --preserve-metadata=identifier,entitlements "$1""
+
+    if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
+      code_sign_cmd="$code_sign_cmd &"
+    fi
+    echo "$code_sign_cmd"
+    eval "$code_sign_cmd"
   fi
 }
 
@@ -86,6 +91,7 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/AlamofireImage/AlamofireImage.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/CVCalendar/CVCalendar.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Cosmos/Cosmos.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Device/Device.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Localize-Swift/Localize_Swift.framework"
@@ -94,8 +100,12 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/AlamofireImage/AlamofireImage.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/CVCalendar/CVCalendar.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Cosmos/Cosmos.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Device/Device.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Localize-Swift/Localize_Swift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SWRevealViewController/SWRevealViewController.framework"
+fi
+if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
+  wait
 fi
