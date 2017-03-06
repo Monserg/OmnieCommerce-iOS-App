e0d7d5a6f626f3b54c6dcd5001af23a00f1c9b55
SerhiiM <msm72@mail.ru>
2017-03-06 20:11:12 +0200

    create new Label class

---
+++ OmnieCommerce/Common/UILabels/UbuntuLightDarkCyanLabel.swift
@@ -0,0 +1,36 @@
+//
+//  UbuntuLightDarkCyanLabel.swift
+//  OmnieCommerce
+//
+//  Created by msm72 on 06.03.17.
+//  Copyright © 2017 Omniesoft. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class UbuntuLightDarkCyanLabel: UILabel {
+    // MARK: - Class Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        didSetup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        didSetup()
+    }
+    
+    
+    // MARK: - Custom Functions
+    func didSetup() {
+        text            =   (text == "Copyright") ? "\u{00A9} Omniesoft, 2016" : text?.localized()
+        font            =   (isAppThemeDark) ? UIFont.systemFont(ofSize: 9) : UIFont(name: "Ubuntu-Light", size: font!.pointSize)!
+        textColor       =   (isAppThemeDark) ? UIColor.white : UIColor.darkCyan
+        textAlignment   =   textAlignment
+        
+        self.adjustsFontSizeToFitWidth  =   true
+        clipsToBounds   =   true
+    }
+}
