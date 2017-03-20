3137cd2a90f60bf817b9eb2e4f172e405fd148f5
SerhiiM <msm72@mail.ru>
2017-03-20 12:19:18 +0200

    fix Templates get info func

--- OmnieCommerce/Scenes/PersonalPageShow/UIView/UserTemplateTableViewCell.swift
+++ OmnieCommerce/Scenes/PersonalPageShow/UIView/UserTemplateTableViewCell.swift
@@ -11,12 +11,12 @@ import AlamofireImage
 
 class UserTemplateTableViewCell: UITableViewCell {
     // MARK: - Properties
-    let expandedHeight: CGFloat     =   308.0
-    let servicesCount               =   4
+    let expandedHeight: CGFloat = 308.0
+    let servicesCount = 4
     
-    var isExpanded: Bool            =   false {
+    var isExpanded: Bool = false {
         didSet {
-            self.dottedBorderViewHeightConstraint.constant  =   (self.isExpanded) ? (self.expandedHeight - 86.0 + CGFloat(self.servicesCount * 17)) : 43.5
+            self.dottedBorderViewHeightConstraint.constant = (self.isExpanded) ? (self.expandedHeight - 86.0 + CGFloat(self.servicesCount * 17)) : 43.5
             self.dottedBorderView.layoutIfNeeded()
 //            self.handlerSendButtonCompletion!()
         }
@@ -34,9 +34,9 @@ class UserTemplateTableViewCell: UITableViewCell {
     
     @IBOutlet weak var commentTextLabel: UbuntuLightVeryLightGrayLabel! {
         didSet {
-            commentTextLabel.lineBreakMode                  =   .byTruncatingTail
-            commentTextLabel.numberOfLines                  =   2
-            commentTextLabel.adjustsFontSizeToFitWidth      =   false
+            commentTextLabel.lineBreakMode = .byTruncatingTail
+            commentTextLabel.numberOfLines = 2
+            commentTextLabel.adjustsFontSizeToFitWidth = false
         }
     }
     
@@ -74,11 +74,11 @@ class UserTemplateTableViewCell: UITableViewCell {
 // MARK: - ConfigureCell
 extension UserTemplateTableViewCell: ConfigureCell {
     func setup(withItem item: Any, andIndexPath indexPath: IndexPath) {
-        let organization                =   item as! Organization
-        nameLabel.text                  =   organization.name
-        categoryLabel.text              =   organization.category.name!
-        commentTextLabel.text           =   "dasd as das asd asdhasgdh  ashhd gahg ha ajhgd hagd haghs  ajhgdhasgd gags"
-        selectionStyle                  =   .none
+        let organization = item as! Organization
+        nameLabel.text = organization.name
+        categoryLabel.text = "Tested" //organization.category.name!
+        commentTextLabel.text = "dasd as das asd asdhasgdh  ashhd gahg ha ajhgd hagd haghs  ajhgdhasgd gags"
+        selectionStyle = .none
         
         logoImageView.af_setImage(withURL: URL(string: organization.logoURL ?? "https://blog.testfort.com/wp-content/uploads/2015/07/apple_logo.png")!,
                                   placeholderImage: UIImage.init(named: "image-no-photo"),
