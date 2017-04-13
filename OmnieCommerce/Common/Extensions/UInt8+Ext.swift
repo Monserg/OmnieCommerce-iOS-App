0e596c9487b94999ac268c6ef07eee68917aded5
SerhiiM <msm72@mail.ru>
2017-04-13 15:11:17 +0300

    modify schedules array

--- OmnieCommerce/Common/Extensions/UInt8+Ext.swift
+++ OmnieCommerce/Common/Extensions/UInt8+Ext.swift
@@ -17,23 +17,33 @@ extension UInt8 {
         weekdayShort.insert(element, at: weekdayShort.count)
         let counts: [UInt8] = [1, 2, 4, 8, 16, 32, 64]
         var result = String()
-        var ones = [String]()
-        var zeros = [String]()
+        var hitch = [String]()
+        var comma = [String]()
+        var lastIndex = 0
         
         for (index, count) in counts.enumerated() {
             let dayCode = self & count
-            (dayCode > 0) ? ones.append(weekdayShort[index]) : zeros.append(weekdayShort[index])
+            
+            if (dayCode > 0) {
+                (index - lastIndex == 1) ? hitch.append(weekdayShort[index]) : comma.append(weekdayShort[index] + ", ")
+                lastIndex = index
+            }
         }
         
-        if (ones.count == 1) {
-            result = ones.first!
-        } else if (ones.count > 1) {
-            result = ones.first! + " - " + ones.last!
+        if (hitch.count > 0 && comma.count > 0) {
+            comma.append(contentsOf: hitch.map { $0 + ", "})
+            result = comma.reduce(result, { $0 + $1 })
+            result = result.substring(to: result.index(result.endIndex, offsetBy: -2))
+        } else if (hitch.count == 1) {
+            result = hitch.first!
+        } else if (hitch.count > 1) {
+            result = hitch.first! + " - " + hitch.last!
+        } else if (comma.count == 1) {
+            result = comma.first!
+        } else if (comma.count > 0) {
+            result = comma.reduce(result, { $0 + $1 })
+            result = result.substring(to: result.index(result.endIndex, offsetBy: -2))
         }
-        
-//        else if (zeros.count > 0) {
-//            result =
-//        }
         
         return result
     }
